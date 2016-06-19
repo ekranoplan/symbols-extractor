959db102bb370397a1b8d479fd5bef76d7676185
ekranoplan <hiroki.miura.94@gmail.com>
2016-06-19 18:08:32 +0900

    add: code

---
+++ src/symbols/Main.java
@@ -0,0 +1,46 @@
+package symbols;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by user on 2016/06/18.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        List<String> symbols = new ArrayList<>();
+        symbols.add("。");
+        symbols.add("、");
+        symbols.add("「");
+        symbols.add("」");
+        symbols.add("（");
+        symbols.add("）");
+        symbols.add("…");
+        
+        String str = loadFile("./data/sangetsuki.txt");
+        String c;
+        for (int i = 0; i < str.length(); i++) {
+            c = String.valueOf(str.charAt(i));
+            if (symbols.contains(c)) {
+                System.out.print(c);
+            }
+        }
+        
+    }
+    
+    private static String loadFile(String filePath) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
+        String str;
+        StringBuilder sb = new StringBuilder();
+        while((str = br.readLine()) != null){
+            sb.append(str);
+        }
+        br.close();
+        return sb.toString();
+    }
+
+}
