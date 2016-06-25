90d31a4158204a9b6fd8b7f2352d469e86f26d46
ekranoplan <hiroki.miura.94@gmail.com>
2016-06-25 18:16:51 +0900

    抽出する記号を外部リスト化

--- src/symbols/Main.java
+++ src/symbols/Main.java
@@ -8,21 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by user on 2016/06/18.
+ * Created by ekranoplan on 2016/06/18.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<String> symbols = new ArrayList<>();
-        symbols.add("。");
-        symbols.add("、");
-        symbols.add("「");
-        symbols.add("」");
-        symbols.add("（");
-        symbols.add("）");
-        symbols.add("…");
-        
-        String str = loadFile("./data/sangetsuki.txt");
+        List<String> symbols = loadSymbols("./data/symbollist.txt");
+        String str = loadFile("./data/sample.txt");
         String c;
+
         for (int i = 0; i < str.length(); i++) {
             c = String.valueOf(str.charAt(i));
             if (symbols.contains(c)) {
@@ -43,4 +36,15 @@ public class Main {
         return sb.toString();
     }
 
+    private static List<String> loadSymbols(String filePath) throws IOException {
+        List<String> list = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
+        String str;
+        while ((str = br.readLine()) != null) {
+            list.add(str);
+        }
+        br.close();
+        return list;
+    }
+    
 }
